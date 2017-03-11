ade0e78d8d76b27f3ef868d3a1565fc0936666bd
Misanovy <misanovy@gmail.com>
2017-03-11 22:15:31 +0100

    Implementaiton of Substraction node and Substraction node Tests

---
+++ Math/Nodes/Functions/Binary/SubstractionNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math.Nodes.Functions.Binary
+{
+    public class SubstractionNode : IBinaryOperationNode
+    {
+        public INode RightNode { get; set; }
+        public INode LeftNode { get; set; }
+
+        public double Evaluate()
+        {
+            return LeftNode.Evaluate() - RightNode.Evaluate();
+        }
+    }
+}
---
+++ MathTests/SubstractionNodeTests.cs
@@ -0,0 +1,25 @@
+﻿using Math.Nodes;
+using Math.Nodes.Functions.Binary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathTests
+{
+    [TestClass]
+    public class SubstractionNodeTests
+    {
+        [TestMethod]
+        public void BasicSubstractionTest()
+        {
+            SubstractionNode substraction = new SubstractionNode();
+            substraction.LeftNode = new NumberNode(3.5);
+            substraction.RightNode = new NumberNode(1.3);
+
+            Assert.AreEqual(2.2, substraction.Evaluate());
+        }
+    }
+}
