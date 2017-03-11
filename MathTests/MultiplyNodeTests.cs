9b58612d31569334cae14ba03b53784251512e55
Misanovy <misanovy@gmail.com>
2017-03-11 21:49:52 +0100

    Implementaiton of Multiplication node and Multiplication node Tests

---
+++ MathTests/MultiplyNodeTests.cs
@@ -0,0 +1,25 @@
+﻿using Math.Nodes;
+using Math.Nodes.Functions.Binary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathTests
+{
+    [TestClass]
+    public class MultiplyNodeTests
+    {
+        [TestMethod]
+        public void BasicMultiplicationTest()
+        {
+            MultiplyNode multiplication = new MultiplyNode();
+            multiplication.LeftNode = new NumberNode(1.2);
+            multiplication.RightNode = new NumberNode(2.3);
+
+            Assert.AreEqual(2.76, multiplication.Evaluate());
+        }
+    }
+}
