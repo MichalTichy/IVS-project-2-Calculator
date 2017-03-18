ee16a9ad159497d7da0fbb9750c39729a6348a6e
Michal Tichý <michal.tichy@riganti.cz>
2017-03-18 01:52:04 +0100

    Implemented first version of tokennizer

---
+++ Math/Nodes/TemporaryNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math.Nodes
+{
+    public class TemporaryNode: INode
+    {
+        public decimal Evaluate()
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
