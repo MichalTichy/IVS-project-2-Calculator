e34ff4aa7c4505185e9c8c022f6b336003fba08c
Michal Tichý <michal.tichy@riganti.cz>
2017-04-01 16:33:30 +0200

    Moved FactorialNode to its own file

---
+++ Math/Nodes/Functions/Unary/FactorialNode.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace Math.Nodes.Functions.Unary
+{
+    public class FactorialNode : IUnaryOperationNode
+    {
+        public INode Parent { get; set; }
+        public INode ChildNode { get; set; }
+
+        public decimal Evaluate()
+        {
+            var childNodeValue = ChildNode.Evaluate();
+
+            CheckIfItsPossibleToCalculateFactorial(childNodeValue);
+
+            decimal factorial = 1;
+            for (decimal i = 1; i <= childNodeValue; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+
+        void CheckIfItsPossibleToCalculateFactorial(decimal value)
+        {
+            if (value%1>0)
+            {
+                throw new ArithmeticException("It`s not possible to calculate Factorial of decimal number.");
+            }
+
+            if (value>27)
+            {
+                throw new OverflowException("Cannot calculate factorial of bigger number than 27.");
+            }
+
+            if (value<0)
+            {
+                throw new ArgumentException("Cannot calculate factorial of negative number");
+            }
+        }
+
+    }
+}
