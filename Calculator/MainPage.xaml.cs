160fccc45056af361b44e7aa21083056228158ba
GabrielMastny <mastny.gabriel@gmail.com>
2017-04-06 15:17:07 +0200

    Added Buissnes layer

--- Calculator/MainPage.xaml.cs
+++ Calculator/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,10 @@ namespace Calculator
         public MainPage()
         {
             this.InitializeComponent();
+            ApplicationView.PreferredLaunchViewSize = new Size(400, 800);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            var viewModel = new Calculator.MainPage_ViewModel();
+            DataContext = viewModel;
         }
     }
 }
---
+++ Calculator/MainPage_ViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class MainPage_ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string val = "ggg";
+        private string result = "0";
+        public string Values
+        {
+            get { return val; }
+            set { val = value; }
+        }
+
+        public string Result
+        {
+            get { return result; }
+            set { result = value; }
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
