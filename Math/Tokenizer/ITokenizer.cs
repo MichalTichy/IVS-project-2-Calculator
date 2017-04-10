a6d5aeb8511df81d10dccf43d7860621459cc5fb
Michal Tichý <michal.tichy@riganti.cz>
2017-04-10 13:20:49 +0200

    Added all public functions from tokenizer to underling interface

--- Math/Tokenizer/ITokenizer.cs
+++ Math/Tokenizer/ITokenizer.cs
@@ -5,8 +5,10 @@ namespace Math
     public interface ITokenizer
     {
         IReadOnlyCollection<MathOperatorDescription> RegisteredOperators { get; }
-        void RegisterOperator(MathOperatorDescription operatorDescription);
+
         ICollection<(string token, MathOperatorDescription operatorDescription)> AssignOperatorDescriptionToTokens(ICollection<string> tokens);
+        ICollection<MathOperatorDescription> GetPossibleNextMathOperators(ExpressionPartTypes? previousExpressionPart);
+        void RegisterOperator(MathOperatorDescription operatorDescription);
         ICollection<string> SplitExpressionToTokens(string expression);
     }
 }
