5983d5e2b232d7b3ee5795f3cb4fe1d7b7559563
GabrielMastny <mastny.gabriel@gmail.com>
2017-04-11 22:43:41 +0200

    Managed complete input handling

--- Calculator/ViewModels/MainPage_ViewModel.cs
+++ Calculator/ViewModels/MainPage_ViewModel.cs
@@ -5,9 +5,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Math;
+using Math.Nodes.Functions;
+using Math.Nodes.Values;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
+
 
 namespace Calculator
 {
@@ -17,31 +20,43 @@ namespace Calculator
         public MainPage_ViewModel()
         {
             tk = new Tokenizer();
-            lst = tk.RegisteredOperators;
-            node = new Math.Nodes.Values.NumberNode("0");
-            Result = node.Evaluate().ToString();
+            lst = tk.GetPossibleNextMathOperators(ExpressionPartTypes.Number);
+            extree = new ExpressionTreeBuilder<Math.Tokenizer>((Tokenizer)tk);
+            
         }
+        private MathOperatorDescription selectedItem;
         private Math.ITokenizer tk;
-        private ICollection<MathOperatorDescription> test;
-        private IReadOnlyCollection<MathOperatorDescription> lst;
+        private ICollection<string> collectionWithExpressionPartTypes;
+        private IExpressionTreeBuilder extree;
+        private ICollection<MathOperatorDescription> lst;
         private Math.Nodes.INode node;
-        private string val = "0";
+        private string val = "";
         private string result;
+        private bool isParsable;
+        private ICollection<(string, MathOperatorDescription)> ts2;
+        private int selection;
+
 
 
         public string Values
         {
-            get { return val; }
-            set { val = value;
+            get => val; 
+            set
+            {
+                val = value;
+                evaluate(true);
                 OnPropertyChanged("Values");
-                something();
-                }
+            }
         }
 
-        public IReadOnlyCollection<MathOperatorDescription> Lst
+        public ICollection<MathOperatorDescription> Lst
         {
-            get { return lst; }
-            private set { }
+            get => lst;
+            set
+            {
+                lst = value;
+                OnPropertyChanged("Lst");
+            }
         }
 
         public string Result
@@ -50,6 +65,32 @@ namespace Calculator
             set { result = value;
                 OnPropertyChanged("Result");
                         }
+        }
+
+        public MathOperatorDescription SelectedItem
+        { get => selectedItem;
+          set
+          {
+                Debug.WriteLine("tets");
+                if (value != selectedItem)
+                {
+                    selectedItem = value;
+                    Values += SelectedItem.TextRepresentation;
+                    evaluate(true);
+                    OnPropertyChanged("SelectedItem");
+                    
+                }
+          }
+        }
+
+        public int Selection
+        {
+            get => selection;
+            set
+            {
+                selection = value;
+                OnPropertyChanged("Selection");
+            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -63,8 +104,70 @@ namespace Calculator
 
     public void something()
         {
-            Result = Values;
-            Debug.WriteLine($"result: {result}, Result: {Result}, value: {val}, Value: {Values}");
+            evaluate(false);
+        }
+
+        private void evaluate(bool edit)
+        {
+            string valueToEvalution = val;
+            if (edit)
+            {
+                valueToEvalution = val.Substring(0, selection);
+            }
+
+            try
+            {
+                isParsable = true;
+                
+                node = extree.ParseExpression(valueToEvalution);
+                collectionWithExpressionPartTypes = tk.SplitExpressionToTokens(valueToEvalution);
+               
+                
+                ts2 = tk.AssignOperatorDescriptionToTokens(collectionWithExpressionPartTypes);
+
+            }
+            catch
+            {
+                isParsable = false;
+                Debug.WriteLine("catch");
+            }
+            finally
+            {
+                Debug.WriteLine($"last expr {collectionWithExpressionPartTypes.Last<string>()}");
+                Lst = tk.GetPossibleNextMathOperators(Tokenizer.GetPrecedingExpressionPartType(ts2.Last<(string, MathOperatorDescription)>()));
+                Debug.WriteLine($"test {ts2.Last<(string, MathOperatorDescription)>().Item1}");
+                if (isParsable)
+                {
+                    int totalyNonUsefull;
+                    if (int.TryParse(collectionWithExpressionPartTypes.Last<string>(), out totalyNonUsefull))
+                    {
+                        
+                        Result = node.Evaluate().ToString();
+                    }
+                }
+                else
+                {
+                    Lst = null;
+                }
+            }
+            
+
+
+            
+           
+
+
+        }
+        public void SelectionChanged()
+        {
+            Debug.WriteLine($"xxxx {selection} .. {Values.Length}");
+            if (selection != Values.Length)
+            {
+                evaluate(true);
+            }
+            else evaluate(false);
+
+
         }
 
         
